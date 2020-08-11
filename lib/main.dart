c878ebbf719a83f5d5fd25aed41bcd2c32c26743
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-08-10 23:24:33 -0400

    picture asset added

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,14 @@ class _MyAppState extends State<MyApp> {
   VideoPlayerController _controller;
 
   @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
   void initState() {
     super.initState();
-
     _controller = VideoPlayerController.asset("assets/sunset.mp4")
     ..initialize().then((_){
       _controller.play();
@@ -43,11 +48,81 @@ class _MyAppState extends State<MyApp> {
                     child: VideoPlayer(_controller),
                   ),
                 ),
-              )
+              ),
+              LoginWidget()
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+// A basic login widget with a logo and a form with rounded corners.
+class LoginWidget extends StatelessWidget {
+  const LoginWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          child: Center(
+            child: Image(
+              image: AssetImage("assets/coffee_logo.png"),
+              width: 200.0,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 50.0,
+        ),
+        Container(
+          decoration: new BoxDecoration(
+            color: Colors.white.withAlpha(200),
+            borderRadius: new BorderRadius.only(
+              topLeft: const Radius.circular(10.0),
+              topRight: const Radius.circular(10.0),
+              bottomLeft: const Radius.circular(10.0),
+              bottomRight: const Radius.circular(10.0),
+            ),
+          ),
+          padding: EdgeInsets.all(16),
+          width: 300,
+          height: 200,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              TextField(
+                cursorColor: Color(0xffb55e28),
+                decoration: InputDecoration(
+                  hintText: 'Username',
+                ),
+              ),
+              TextField(
+                cursorColor: Color(0xffb55e28),
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                ),
+              ),
+              ButtonTheme(
+                minWidth: 300.0,
+                child: RaisedButton(
+                  color: Color(0xffb55e28),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Color(0xffffd544), fontSize: 20),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
